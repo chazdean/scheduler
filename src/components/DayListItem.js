37e9b07740a49a9f52546184729cdf6c296f89a2
Chaz Dean <cdean00@gmail.com>
2021-11-13 15:28:23 -0500

    add componant conditionals

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,20 @@ export default function DayListItem(props) {
     'day-list__item--full': props.spots == 0
   })
 
+  const formatSpots = (props) => {
+    return (
+      <div>
+        {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+        {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+        {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      </div>
+    );
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      {formatSpots(props)}
     </li>
   );
 }
