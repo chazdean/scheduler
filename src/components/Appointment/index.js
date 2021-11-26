af95f33e6f9c901e782b9da05d3f8ef1354ba8d3
Chaz Dean <cdean00@gmail.com>
2021-11-25 19:47:26 -0500

    edit promises to show error pages when needed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,19 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Error from "./Error";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE"
-const SAVING = "SAVING"
+const LOADING = "LOADING"
+const EDIT = "EDIT"
+const CONFIRM = "CONFIRM"
+const ERROR_SAVE = "ERROR_SAVE"
+const ERROR_DELETE = "ERROR_DELETE"
 
 export default function Appointment(props) {
 
@@ -27,21 +33,35 @@ export default function Appointment(props) {
     };
 
     setMessage("Saving")
-    transition(SAVING)
+    transition(LOADING)
     
     props.bookInterview(props.id, interview)
-    .then(() => {
+    .then((error) => {
+      console.log("THIS IS ERROR{{", error)
+      if (error) {
+        transition(ERROR_SAVE, true)
+      } else {
       transition(SHOW)
+    }
     })
   }
 
   function cancel() {
     setMessage("Deleting")
-    transition(SAVING)
+    transition(LOADING)
     props.cancelInterview(props.id)
-    .then(() => {
+    .then((error) => {
+      console.log("THIS IS ERROR{{", error)
+      if (error) {
+        transition(ERROR_DELETE, true)
+      } else {
       transition(EMPTY)
+      }
     })
+  }
+
+  function edit() {
+    transition(EDIT)
   }
 
   return (
@@ -52,7 +72,8 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          delete={cancel}
+          onDelete={()=> transition(CONFIRM)}
+          onEdit={edit}
         />
       )} 
       {mode === CREATE && (
@@ -62,9 +83,35 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
-      {mode === SAVING && (
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel={() => {console.log("Clicked Cancel"); back()}}
+          onSave={save}
+        />
+      )}
+      {mode === LOADING && (
         <Status
           message={message}
+        />
+      )} 
+      {mode === CONFIRM && (
+        <Confirm
+          message={"Are you sure you want to delete?"}
+          onConfirm={cancel}
+          onCancel={() => {console.log("Clicked Cancel"); back()}}
+        />
+      )} 
+      {mode === ERROR_SAVE && (
+        <Error
+          onClose={() => {console.log("Clicked Cancel"); back()}}
+        />
+      )} 
+      {mode === ERROR_DELETE && (
+        <Error
+          onClose={() => {console.log("Clicked Cancel"); back()}}
         />
       )} 
     </article>
