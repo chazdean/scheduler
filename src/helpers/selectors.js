b850e404ac8d1a80d47e11b761a6ccf9b24af821
Chaz Dean <cdean00@gmail.com>
2021-11-28 13:48:05 -0500

    Add comments and correct syntax

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,4 @@
+// Returns an array of appointments for the given day
 export function getAppointmentsForDay(state, day) {
  
 const appointmentsForDay = [];
@@ -19,6 +20,7 @@ for (const key in state.appointments) {
 return appointmentList;
 }
 
+// Returns a new object containing the interview data when passed an object that contains the interviewer
 export function getInterview(state, interview) {
   const newInterviewObject = interview;
 
@@ -32,6 +34,7 @@ export function getInterview(state, interview) {
   return newInterviewObject
 }
 
+// Return an array of interviewers for the given day
 export function getInterviewersForDay(state, day) {
   const interviewsForDay = [];
   const interviewList = [];
@@ -50,4 +53,25 @@ for (const key in state.interviewers) {
 }
 
 return interviewList;
+}
+
+// Returns the number of spots available for a given day
+export function getNumberOfSpots(state, day) {
+  const appointmentsForDay = [];
+  let spots = 0;
+
+  for (const dayItem of state.days) {
+    if (dayItem.name === day) {
+      appointmentsForDay.push(...dayItem.appointments);
+    }
+  }
+
+  for (const key of appointmentsForDay) {
+    console.log("KEY", key)
+    if (!state.appointments[key].interview) {
+      spots ++
+    }
+  }
+
+return spots;
 }
